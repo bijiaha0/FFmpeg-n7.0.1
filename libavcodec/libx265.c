275ed8a0f492518f2ff05979a6443ad200cf4864
jiahaobi <jiahaobi@sohu-inc.com>
2024-09-24 17:01:33 +0800

    dolby

--- libavcodec/libx265.c
+++ libavcodec/libx265.c
@@ -794,16 +794,21 @@ static int libx265_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
             }
         }
 
-        for (int i = 0; i < pic->nb_side_data; i++)
-        {
-            const AVFrameSideData *side_data = pic->side_data[i];
-            if (side_data->type == AV_FRAME_DATA_DOVI_RPU_BUFFER)
+        if (passthru_dynamic_hdr_metadata & DOVI){
+
+            for (int i = 0; i < pic->nb_side_data; i++)
             {
-                x265_dolby_vision_rpu *rpu = &x265pic.rpu;
-                rpu->payload = side_data->data;
-                rpu->payloadSize = side_data->size;
+                const AVFrameSideData *side_data = pic->side_data[i];
+                if (side_data->type == AV_FRAME_DATA_DOVI_RPU_BUFFER)
+                {
+                    x265_dolby_vision_rpu *rpu = &x265pic.rpu;
+                    rpu->payload = side_data->data;
+                    rpu->payloadSize = side_data->size;
+                }
             }
+
         }
+
     }
 
     ret = ctx->api->encoder_encode(ctx->encoder, &nal, &nnal,
